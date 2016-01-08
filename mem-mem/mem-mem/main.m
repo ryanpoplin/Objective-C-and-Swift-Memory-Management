c5d9b34565ccc2066412e503b0a6715c1fff799c
Byrdann Fox <mobileupstate@gmail.com>
2016-01-08 12:57:57 -0500

    examples

--- mem-mem/mem-mem/main.m
+++ mem-mem/mem-mem/main.m
@@ -10,7 +10,39 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+	
+	/* std. memory management example */
+	
+	// SomeObject *myOwnObject;
+	
+	// myOwnObject is created in main
+	// myOwnObject = [[SomeObject alloc] init];
+	
+	// myOwnObject can be used by other objects
+	
+	// [anotherObject using:myOwnObject];
+	
+	// NSMutableArray *myArray;
+	// add my object to myArray
+	// myArray = [[NSMutableArray alloc] initWithObjects:myOwnObject, nil];
+	
+	// but main is responsible for releasing it, and now that an NSMutableArray was initialized with a reference to the initialized myOwnObject, 'main' does not need a reference to it anymore
+	// [myOwnObject release];
+	
+	// however, the array holding a reference to myOwnObject is still needed
+	// [anotherObject usingArray: myArray];
+	
+	// on array destruction every object inside array gets release messaged
+	// [myArray release];
+	
+	// myOwnObject retain count decreases this time to 0 and myOwnObject will be deleted together with the array
+	
+	/* std. memory leak example */
+	
+	// NSMutableString *str = [[NSMutableString alloc] initWithString: @"Leaky"];
+	
 	@autoreleasepool {
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
+	
 }
