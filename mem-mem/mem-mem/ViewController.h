10d08e1939b9505c98827946715289841f91128a
Byrdann Fox <mobileupstate@gmail.com>
2016-01-08 11:42:06 -0500

    init

---
+++ mem-mem/mem-mem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mem-mem
+//
+//  Created by Byrdann Fox on 1/8/16.
+//  Copyright © 2016 Loopi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mem-mem/mem-memUITests/mem_memUITests.m
@@ -0,0 +1,40 @@
+//
+//  mem_memUITests.m
+//  mem-memUITests
+//
+//  Created by Byrdann Fox on 1/8/16.
+//  Copyright © 2016 Loopi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mem_memUITests : XCTestCase
+
+@end
+
+@implementation mem_memUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
